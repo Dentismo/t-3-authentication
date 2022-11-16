4c6f4cd368cafc7abfcfb8be1fbdaf3803f4b56c
zsolnai <georg.zsolnai123@gmail.com>
2022-11-16 15:18:15 +0100

    #1 Add eslint package and redefine ecmaVersion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    "env": {
-        "node": true
+    'env': {
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 6
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 8
     },
-    "rules": {
-        "no-console": "off",
-        "indent": [
-            "error",
+    'rules': {
+        'no-console': 'off',
+        'indent': [
+            'error',
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'windows'
         ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ]
     }
 };  
