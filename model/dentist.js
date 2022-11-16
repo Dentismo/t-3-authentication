102ee0ffdcf127837eafbc78ad75b1a7523beabc
zsolnai <georg.zsolnai123@gmail.com>
2022-11-16 15:28:08 +0100

    #1 Update schema to reflect global schema

--- model/dentist.js
+++ model/dentist.js
@@ -2,10 +2,30 @@ const mongoose = require('mongoose');
 
 const dentistSchema = new mongoose.Schema({
     clinicId: {type: String},
-    name: {type: String},
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true},
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\w+(.-?\w+)@\w+(.-?\w+)(.\w{2,3})+$/
+    },
     token: {type: String}
 });
 
