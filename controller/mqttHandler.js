666d512ed701916edfa3d92fef8f8c0da60f3d5a
ansis <gusplean@student.gu.se>
2023-01-06 19:13:05 +0200

    Fix bookings not displaying

--- controller/mqttHandler.js
+++ controller/mqttHandler.js
@@ -1,54 +1,54 @@
-const mqtt = require('mqtt');
-const login = require('./loginController');
+const mqtt = require('mqtt')
+const login = require('./loginController')
 
 class MqttHandler {
   constructor() {
-    this.mqttClient = null;
-    this.host = 'http://localhost:1883';
-    this.username = 'YOUR_USER'; // mqtt credentials if these are needed to connect
-    this.password = 'YOUR_PASSWORD';
-    this.subscribeTopic = 'request/login/+';
+    this.mqttClient = null
+    this.host = 'http://localhost:1883'
+    this.username = 'YOUR_USER' // mqtt credentials if these are needed to connect
+    this.password = 'YOUR_PASSWORD'
+    this.subscribeTopic = 'request/login/+'
   }
 
   connect() {
     // Connect mqtt with credentials (in case of needed, otherwise we can omit 2nd param)
     try {
-      this.mqttClient = mqtt.connect(this.host, { username: this.username, password: this.password });
+      this.mqttClient = mqtt.connect(this.host, {
+        username: this.username,
+        password: this.password
+      })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
 
-    const localMqttClient = this.mqttClient;
+    const localMqttClient = this.mqttClient
 
     // Mqtt error calback
     this.mqttClient.on('error', (err) => {
-      console.log(err);
-      this.mqttClient.end();
-    });
+      console.log(err)
+      this.mqttClient.end()
+    })
 
     // Connection callback
     this.mqttClient.on('connect', () => {
-      console.log(`Mqtt Client connected, Subscribed to '${this.subscribeTopic}'`);
-    });
+      console.log(
+        `Mqtt Client connected, Subscribed to '${this.subscribeTopic}'`
+      )
+    })
 
     // Subscribe the 'request/login' to receive login requests from dentists
-    this.mqttClient.subscribe(this.subscribeTopic, {qos: 1});
+    this.mqttClient.subscribe(this.subscribeTopic, { qos: 1 })
 
     // When a message arrives, console.log it
     this.mqttClient.on('message', async function (topic, message) {
-
       //-------------------------------------------------------------------\\
-      const id = topic.split('/')[2];  //  get id from the topic
+      const id = topic.split('/')[2] //  get id from the topic
       //--------------------------------------------------------------------\\
 
-      console.log(message.toString());
-      const response = await login(JSON.parse(message.toString()));
-      localMqttClient.publish(`response/login/${id}`, JSON.stringify(response));
-      console.log(JSON.stringify(response));
-    });
-
+      const response = await login(JSON.parse(message.toString()))
+      localMqttClient.publish(`response/login/${id}`, JSON.stringify(response))
+    })
   }
 }
 
-
-module.exports = MqttHandler;
+module.exports = MqttHandler
